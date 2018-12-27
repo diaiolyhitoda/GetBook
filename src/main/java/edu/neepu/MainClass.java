45dfdbc82de8665eec88fe64d6cf0e65476ec48e
diaiolyhitoda <magu9711208952@126.com>
2018-12-27 21:14:10 +0800

    测试

---
+++ src/main/java/edu/neepu/MainClass.java
@@ -0,0 +1,37 @@
+package edu.neepu;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainClass {
+
+
+    @Test
+    public void test12() throws IOException {
+        for (int i = 1; i <=120; i++) {
+            Document document = Jsoup.connect("http://www.shicimingju.com/book/sanguoyanyi/"+i+".html").header("Accept-language", "zh-cn").get();
+            Elements elements = document.select(".layui-col-md8");
+            for (Element element : elements) {
+                Element a = element.select(".www-main-container").first();
+                if (a != null) {
+                    System.out.println(a.text());
+                }
+
+                System.out.println("-----------------");
+            }
+
+        }
+
+    }
+
+}
+
+
+
