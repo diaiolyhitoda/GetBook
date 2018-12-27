0f3aff07b114b64cd20d007d5fdb7b104fc4f6a3
diaiolyhitoda <magu9711208952@126.com>
2018-12-27 21:48:33 +0800

    实现了将第一整本书保存至txt文件

--- src/main/java/edu/neepu/MainClass.java
+++ src/main/java/edu/neepu/MainClass.java
@@ -17,33 +17,31 @@ public class MainClass {
 
     @Test
     public void test12() throws IOException {
-        String str = "测试保存文件流";
+
         File file;//创建文件夹
         FileOutputStream stream = null;//new文件流
-        try {
-            file = new File("C:/Users/Administrator/Desktop/个人空间/Aa.txt");
-            stream = new FileOutputStream (file);//将文件夹放在文件流中
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            byte[] contentInBytes = str.getBytes();//转化成字节形
-            stream.write(contentInBytes);//写入
-            stream.flush(); //写完之后刷新
-            stream.close();//关闭流
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+
 
         for (int i = 1; i <=120; i++) {
             Document document = Jsoup.connect("http://www.shicimingju.com/book/sanguoyanyi/"+i+".html").header("Accept-language", "zh-cn").get();
             Elements elements = document.select(".layui-col-md8");
             for (Element element : elements) {
                 Element a = element.select(".www-main-container").first();
-                if (a != null) {
-                    System.out.println(a.text());
-                }
 
-                System.out.println("-----------------");
+
+                try {
+                    file = new File("C:/Users/Administrator/Desktop/Aa.txt");
+                    stream = new FileOutputStream (file);//将文件夹放在文件流中
+                    if (!file.exists()) {
+                        file.createNewFile();
+                    }
+                    byte[] contentInBytes = a.text().getBytes();//转化成字节形
+                    stream.write(contentInBytes);//写入
+                    stream.flush(); //写完之后刷新
+                    stream.close();//关闭流
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
 
         }
